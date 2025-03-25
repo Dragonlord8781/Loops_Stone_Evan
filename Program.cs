97546bd27a1c01c9b95e3391bff64069a8bc5c57
Dragonlord8781 <evan.stone638@gmail.com>
2025-03-25 10:46:03 -0500

    Update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Project 8");
+            string sentence = "In 9 days I'll be able to hunt Mizutsune and Zoh Shia again.";
+            int spaceCount = 0; 
+
+            foreach (char c in sentence)
+            {
+                if (c == ' ')
+                {
+                    spaceCount++;
+                }
+            }
+            Console.WriteLine($"In the sentence \"{sentence}\" there are {spaceCount} spaces");
         }
     }
 }
